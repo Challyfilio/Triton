a454ba481df0ca2fd292d5bc4ce27e6747a1dc16
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-12-22 16:57:03 +0800

    Update image_operate.py

--- image_operate.py
+++ image_operate.py
@@ -3,10 +3,10 @@ import glob
 
 i = 0
 if __name__ == '__main__':
-    for jpgfile in glob.glob(r'E:\ai_datasets\data\Tumor\Training\no_tumor\*.jpg'):
+    for jpgfile in glob.glob('work/*.jpg'):
         image = cv2.imread(jpgfile)
         dst = cv2.flip(image, 1)  # 水平
         # cv2.imshow('image', dst)
         # cv2.waitKey()
-        cv2.imwrite('E:\ai_datasets\data\Tumor\Training\out\out{}.jpg'.format(i), dst)
+        cv2.imwrite('work/out/out{}.jpg'.format(i), dst)
         i = i + 1
