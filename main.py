1563bd216b3db32d9b850b7cc2b7de8527cc767e
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-12-29 15:54:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from mindspore import nn
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +12,13 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    milestone = [2, 5, 100]
+    learning_rates = [0.1, 0.05, 0.01]
+    lr = nn.dynamic_lr.natural_exp_decay_lr(learning_rate=0.05,
+                                            decay_rate=0.5,
+                                            total_step=200,
+                                            step_per_epoch=10,
+                                            decay_epoch=10)
+    print(lr)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
