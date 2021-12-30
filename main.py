dc72fbd495c4a96de11b6018dcc75719e9a3cb43
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-12-30 10:24:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from mindspore import nn
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-import mindspore.nn as nn
-import mindspore.dataset as ds
-import mindspore.dataset.vision.c_transforms as CV
-import mindspore.dataset.transforms.c_transforms as C
-from mindspore import dtype as mstype
-from mindspore.train.callback import TimeMonitor
-from mindspore import Model, Tensor, context, load_checkpoint, load_param_into_net
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+'''
+验证模型用
+Challyfilio
+'''
+from mindspore_solution import *
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    # lr = nn.dynamic_lr.natural_exp_decay_lr(learning_rate=0.05,
-    #                                         decay_rate=0.5,
-    #                                         total_step=200,
-    #                                         step_per_epoch=10,
-    #                                         decay_epoch=10)
-    # print(lr)
+    context.set_context(mode=context.GRAPH_MODE, device_target="GPU")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    val_data_path = 'data/Tumor/Testing'
+
+    val_ds = create_dataset(val_data_path, training=False)
+    val_ds = val_ds.batch(batch_size=394, drop_remainder=True)
+
+    class_name = {0: "glioma", 1: "meningioma", 2: "no", 3: 'pituitary'}
+    net = resnet50(class_num=4)
+    num_epochs = 210
+    # image_show(val_ds, class_name)
+
+    lr = 0.0005
+    # 定义优化器和损失函数
+    opt = nn.Adagrad(params=net.trainable_params(), learning_rate=lr, weight_decay=0.05)
+    loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean')  # 交叉熵
+
+    # 实例化模型
+    model = Model(net, loss, opt, metrics={"Accuracy": nn.Accuracy()})
+
+    # 测试模型用
+    net_test(net, 'Luna.ckpt', model, val_ds)
+    visualize_model(net, 'Luna.ckpt', class_name, val_ds)
+
