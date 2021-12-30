0fb1e1f06817a76895ee01ba6beec523fe6a8c0e
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-12-30 21:07:36 +0800

    Update mindspore_solution.py

--- mindspore_solution.py
+++ mindspore_solution.py
@@ -205,15 +205,15 @@ if __name__ == '__main__':
     #         param.requires_grad = False
     # ——————————————
 
-    lr = 0.0005
+    lr = 0.0001
     # 定义优化器和损失函数
     # opt = nn.Momentum(params=net.trainable_params(), learning_rate=lr, momentum=0.9)
     # opt = nn.Adam(params=net.trainable_params(), learning_rate=lr)
     opt = nn.Adagrad(params=net.trainable_params(), learning_rate=lr, weight_decay=0.05)
-    # loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean')  # 交叉熵
-    loss = CrossEntropySmooth(sparse=True, reduction='mean',
-                              smooth_factor=0.1,
-                              num_classes=4)
+    loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean')  # 交叉熵
+    # loss = CrossEntropySmooth(sparse=True, reduction='mean',
+    #                           smooth_factor=0.1,
+    #                           num_classes=4)
 
     # 实例化模型
     model = Model(net, loss, opt, metrics={"Accuracy": nn.Accuracy()})
