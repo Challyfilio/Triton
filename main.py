ce1e604b6c898c9c9bc44ed88a7410c59bc7b2f0
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-12-30 13:40:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from mindspore_solution import *
 
 if __name__ == '__main__':
     val_data_path = 'data/Tumor/Testing'
-    batch_size = 32
+    batch_size = 394
     pred_visualize = False
 
     val_ds = create_dataset(val_data_path, training=False)
     val_ds = val_ds.batch(batch_size=batch_size, drop_remainder=True)
 
     class_name = {0: "glioma", 1: "meningioma", 2: "no", 3: 'pituitary'}
-    image_show(val_ds, class_name)
-    
     net = resnet50(class_num=4)
     lr = 0.0005
     # 定义优化器和损失函数
